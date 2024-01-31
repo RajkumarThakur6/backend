b06e2dbde659a60153cabdff68ce872147ca85ab
Raj <rajkumar.thakur6@gmail.com>
2024-01-31 20:58:25 +0530

    Second commit

---
+++ src/main/java/net/javaguides/ems/dto/EmployeeDto.java
@@ -0,0 +1,19 @@
+package net.javaguides.ems.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.javaguides.ems.entity.Department;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long departmentId;
+}
